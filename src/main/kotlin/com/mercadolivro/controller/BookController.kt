45e6c8df02d0d553d135b33f3ac2332f70d4d8f2
livioqueiroz <queiroz.livio@gmail.com>
2022-02-04 18:41:42 -0300

    92. Melhorias de projeto

--- src/main/kotlin/com/mercadolivro/controller/BookController.kt
+++ src/main/kotlin/com/mercadolivro/controller/BookController.kt
@@ -3,7 +3,9 @@ package com.mercadolivro.controller
 import com.mercadolivro.controller.request.PostBookRequest
 import com.mercadolivro.controller.request.PutBookRequest
 import com.mercadolivro.controller.response.BookResponse
+import com.mercadolivro.controller.response.PageResponse
 import com.mercadolivro.extension.toBookModel
+import com.mercadolivro.extension.toPageResponse
 import com.mercadolivro.extension.toResponse
 import com.mercadolivro.service.BookService
 import com.mercadolivro.service.CustomerService
@@ -16,43 +18,43 @@ import javax.validation.Valid
 
 
 @RestController
-@RequestMapping("book")
+@RequestMapping("books")
 class BookController(
-    val customerService :CustomerService,
-    val bookService : BookService
+    private val customerService: CustomerService,
+    private val bookService: BookService
 ) {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun create(@RequestBody @Valid request: PostBookRequest){
+    fun create(@RequestBody @Valid request: PostBookRequest) {
         val customer = customerService.findById(request.customerId)
         bookService.create(request.toBookModel(customer))
     }
 
     @GetMapping
-    fun findAll(@PageableDefault(page=0, size = 10) pageable: Pageable): Page<BookResponse> {
-       return bookService.findAll(pageable).map { it.toResponse() }
+    fun findAll(@PageableDefault(page = 0, size = 10) pageable: Pageable): PageResponse<BookResponse> {
+        return bookService.findAll(pageable).map { it.toResponse() }.toPageResponse()
     }
 
     @GetMapping("/active")
-    fun findActives(@PageableDefault(page=0, size = 10) pageable: Pageable): Page<BookResponse>{
+    fun findActives(@PageableDefault(page = 0, size = 10) pageable: Pageable): Page<BookResponse> {
         return bookService.findActives(pageable).map { it.toResponse() }
     }
 
     @GetMapping("/{id}")
-    fun findById(@PathVariable id : Int): BookResponse{
+    fun findById(@PathVariable id: Int): BookResponse {
         return bookService.findById(id).toResponse()
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    fun delete(@PathVariable id : Int){
+    fun delete(@PathVariable id: Int) {
         bookService.delete(id)
     }
 
     @PutMapping("/{id")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    fun update(@PathVariable id : Int, @RequestBody book : PutBookRequest){
+    fun update(@PathVariable id: Int, @RequestBody book: PutBookRequest) {
         val bookSaved = bookService.findById(id)
         bookService.update(book.toBookModel(bookSaved))
     }
